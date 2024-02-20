e2f1eed22f9cb9bbfdea717312fae00b26c5d5fb
Nat Gunning <122816248+NatGunning@users.noreply.github.com>
2024-02-20 15:53:34 +0000

    Update Main.java
    Changed temperature values to show rounding function.

--- MediScore/Main.java
+++ MediScore/Main.java
@@ -4,13 +4,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Patient patient1 = new Patient(Patient.AirOrOxygen.Air, Patient.Consciousness.Alert, 15, 95, 37.1f);
+        Patient patient1 = new Patient(Patient.AirOrOxygen.Air, Patient.Consciousness.Alert, 15, 95, 37.10f);
         System.out.println(patient1.toString());
 
         Patient patient2 = new Patient(Patient.AirOrOxygen.Oxygen, Patient.Consciousness.Alert, 17, 95, 37.1f);
         System.out.println(patient2.toString());
 
-        Patient patient3 = new Patient(Patient.AirOrOxygen.Oxygen, Patient.Consciousness.CVPU, 23, 88, 38.5f);
+        Patient patient3 = new Patient(Patient.AirOrOxygen.Oxygen, Patient.Consciousness.CVPU, 23, 88, 38.483f);
         System.out.println(patient3.toString());
     }
 }
